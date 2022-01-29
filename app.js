f0007bb1a53c6a89ab46e7f779f987b1e97cda56
Carlie Smith <carliesmith@amrock.com>
2022-01-29 15:46:38 -0500

    module and template added

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const generatePage = require(`./src/page-template`);
+const profileDataArgs = process.argv.slice(2);
+const [username, github] = profileDataArgs;
+
+
+
+
+fs.writeFile('./index.html', generatePage(username, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output')
+});
