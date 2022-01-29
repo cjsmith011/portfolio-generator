d5600bd3f17034e267994fb90920e6bc5a3d85f7
Carlie Smith <carliesmith@amrock.com>
2022-01-29 18:54:26 -0500

    collecting all data

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const generatePage = require(`./src/page-template`);
 const inquirer = require(`inquirer`);
 const promptUser = () => {
     return inquirer.prompt([
@@ -121,18 +123,18 @@ const promptProject = portfolioData => {
 };
     
 
-// const fs = require('fs');
-// const generatePage = require(`./src/page-template`);
-
-// const pageHTML = generatePage(username, github);
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-//     console.log('Portfolio complete! Check out index.html to see the output')
-// });
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw err;
+        console.log('Portfolio complete! Check out index.html to see the output')
+    });
+    
     });
