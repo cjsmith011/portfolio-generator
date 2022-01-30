b082c529fa46c6b19a8cdf860aa819f75f1d2e27
Carlie Smith <carliesmith@amrock.com>
2022-01-30 13:32:11 -0500

    finished

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site');
 const generatePage = require(`./src/page-template`);
 const inquirer = require(`inquirer`);
 const promptUser = () => {
@@ -129,12 +129,36 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw err;
-        console.log('Portfolio complete! Check out index.html to see the output')
+        return generatePage(portfolioData);
+    })
+    .then (pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then (writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then (copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
+
+    // fs.writeFile('./dist/index.html', pageHTML, err => {
+    //     if (err) {
+    //     console.log(err);
+    //     return;
+    // }
+    //     console.log('Portfolio complete! Check out index.html to see the output');
+
+    //     fs.copyFile('./src/style.css', './dist/style.css',  err => {
+    //         if (err) {
+    //             console.log(err);
+    //             return;
+    //         }
+    //         console.log('Style sheet copied successfully!');
+    //     });
+    // });
     
-    });
+    
